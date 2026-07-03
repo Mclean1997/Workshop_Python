05c8e5bf8fe1f90cf647c96004166f8dafe79780
Mclean1997 <Mcleansemela@gmail.com>
2026-07-03 12:46:17 +0200

    Update 1_📂_Upload.py

--- pages/1_📂_Upload.py
+++ pages/1_📂_Upload.py
@@ -1,1 +1,23 @@
+import streamlit as st
+from utils.loader import load_data
 
+st.title("📂 Upload Dataset")
+
+uploaded = st.file_uploader(
+    "Upload your dataset",
+    type=["csv","xlsx","xls","rds","rdata"]
+)
+
+if uploaded:
+
+    df = load_data(uploaded)
+
+    st.session_state["df"] = df
+
+    st.success("Dataset Loaded Successfully!")
+
+    st.write(df.head())
+
+    st.write("Rows:",df.shape[0])
+
+    st.write("Columns:",df.shape[1])
