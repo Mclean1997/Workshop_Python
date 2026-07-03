38b6af73f480c55b3cc7a51762a064a3ff1d7e35
Mclean1997 <Mcleansemela@gmail.com>
2026-07-03 12:49:50 +0200

    Update 3_⚙️_Settings.py

--- pages/3_⚙️_Settings.py
+++ pages/3_⚙️_Settings.py
@@ -1,1 +1,30 @@
+import streamlit as st
 
+st.title("⚙ Settings")
+
+split = st.slider(
+    "Train Size",
+    0.5,
+    0.9,
+    0.8
+)
+
+seed = st.number_input(
+    "Random Seed",
+    0,
+    99999,
+    42
+)
+
+cv = st.slider(
+    "Cross Validation Folds",
+    2,
+    10,
+    5
+)
+
+st.session_state["split"]=split
+st.session_state["seed"]=seed
+st.session_state["cv"]=cv
+
+st.success("Settings Saved")
